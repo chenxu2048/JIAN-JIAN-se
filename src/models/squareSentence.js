65cfc0121ab40a2c87ba4e96f7dccadda2c4ab3d
BowenWu <bowen.wu@163.com>
2018-03-12 16:21:11 +0800

    添加广场

--- src/models/squareSentence.js
+++ src/models/squareSentence.js
@@ -25,10 +25,14 @@ export async function insertSquareSentences(author_id, sentences, thoughts) {
 
 /** 
  * 获取世界频道
+ * @author 吴博文
 */
 export async function getAllSquareSentences() {
     const sql = `
-    SELECT * FROM square;
+    SELECT square.*, user.nick_name, user.avator_url
+    FROM square INNER JOIN user
+    ON user.user_id = square.author_user_id
+    ;
     `;
     let square_record = await queryDb(sql);
     console.log(square_record);
@@ -38,7 +42,31 @@ export async function getAllSquareSentences() {
 /**
  * 获取某条动态的评论
  * @param {int} square_id 广场中动态的唯一标示
+ * @author 吴博文
  */
 export async function retriveCommentsBySquareId(square_id) {
-
+    const sql = `
+        SELECT comment.content, comment.add_time, user.nick_name, user.avator_url
+        FROM comment INNER JOIN user ON comment.comment_user_id = user.user_id
+        WHERE comment.square_square_id = ?;
+    `;
+    return await queryDb(sql, [square_id]);
 }
+
+/**
+ * 向某条动态插入一条记录
+ * @param {int} square_id 广场动态的标示
+ * @param {String} content 评论的内容
+ * @param {int} user_id 用户的唯一标示
+ * @author 吴博文
+ */
+export async function insertCommentBySquareId(square_id, content, user_id) {
+    const sql = `
+        INSERT INTO comment SET
+        square_square_id = ? AND
+        content = ? AND
+        comment_user_id = ?;
+    `;
+    await queryDb(sql, [square_id, content, user_id]);
+}
+
